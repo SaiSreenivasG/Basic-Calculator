7c1598d2a9347cb926c43c8a56c1dc3d2a49578f
Sai <saisreenivasgantasala@gmail.com>
2026-05-11 20:32:50 +0530

    streamlit version 1

--- appy.py
+++ appy.py
@@ -1,28 +1,38 @@
 from helpers import *
-n1 = int(input("Enter first number"))
-n2 = int(input("Enter second number"))
-print("Select the operation to do on the two numbers")
-print("Type 1 - Multiplication")
-print("Type 2 - Addition")
-print("Type 3 - Divide")
-print("Type 4 - subtraction")
-print("Type 5 - power/exponential")
-operator = int(input("Enter the option from above"))
+import streamlit as st
+
+st.title('Calculator of two numbers')
+
+
+st.title('calculator of two numbers')
+
+#n1 = int(input("Enter first number"))
+n1 = int(st.number_input("Enter first number"))
+#n2 = int(input("Enter second number"))
+n2 = int(st.number_input("Enter second number"))
+st.write("Select the operation to do on the two numbers")
+st.write("Type 1 - Multiplication")
+st.write("Type 2 - Addition")
+st.write("Type 3 - Divide")
+st.write("Type 4 - subtraction")
+st.write("Type 5 - power/exponential")
+#operator = int(input("Enter the option from above"))
+operator = int(st.number_input("Enter the option from above"))
 
 if operator == 1:
-    print(multiply(n1,n2))
+    st.write(multiply(n1,n2))
 elif operator == 2:
-    print(add(n1,n2))
+    st.write(add(n1,n2))
 elif operator == 3:
-    print(divide(n1,n2))
+    st.write(divide(n1,n2))
 elif operator == 4:
-    print(sub(n1,n2))
+    st.writerint(sub(n1,n2))
 elif operator == 5:
-    print(power(n1,n2))
+    st.write(power(n1,n2))
 elif operator == 6:
-    print(floordivision(n1,n2))
+    st.write(floordivision(n1,n2))
 else:
-    print("Please select valid option")
+    st.write("Please select valid option")
 
 
 
